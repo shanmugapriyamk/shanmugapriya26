af2322110f72801907c8d2a23f2f1b330f7d8218
shanmugapriyamk <48437585+shanmugapriyamk@users.noreply.github.com>
2019-08-16 08:30:36 -0700

    Create number.c

---
+++ number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int b, e;
+long long result = 1;
+printf("Enter a base number: ");
+scanf("%d", &b);
+printf("Enter an exponent: ");
+scanf("%d", &e);
+while (e != 0)
+{
+result *= b;
+-e;
+}
+printf("Answer = %lld", result);
+}
