6cee92503a6b742a6414bed54c79f14d9a037b36
shanmugapriyamk <48437585+shanmugapriyamk@users.noreply.github.com>
2019-08-16 15:30:54 +0530

    Create send.c

---
+++ send.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,s,e;
+printf("\n Enter the starting ponit");
+scanf("%d",&s);
+printf("\n Enter the ending ponit");
+scanf("%d",&e);
+for(i=s;i>=e;i--)
+{
+printf("%d\n");
+}
+}
