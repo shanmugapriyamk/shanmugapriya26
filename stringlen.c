f91c0fea1f7edd46f524270fd601031c3446b082
shanmugapriyamk <48437585+shanmugapriyamk@users.noreply.github.com>
2019-08-18 03:41:44 -0700

    Create stringlen.c

---
+++ stringlen.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h.
+int main()
+{
+char str[100];
+int len, space;
+printf( "Enter string:\n ");
+printf("\n");
+gets(str);
+len = strlen(str);
+printf("\n");
+printf("\n No.of characters in string is %d " , len );
+getch();
+}
